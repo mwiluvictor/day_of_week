7acae98d074fdd5aa7194bb523ad522287e1cb1a
Victor Mwilu <mwilumuisyo@gmail.com>
2023-09-25 21:05:11 +0300

    not yet refined, some logic anomalies

--- day_of_week.c
+++ day_of_week.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+
+int  a, b, d, m, y;
+int i, sum, new, n_sum;
 
 int main()
 {
-	int  a, b, d, m, y;
-	int i, sum, new, n_sum;
-	printf("OBTAIN THE DAY OF THE WEEK OF A GIVEN DATE:\n\n"); 
-	printf("PLEASE ENTER THE DATE IN THE FORMAT DD-MM-YYYY:\n"); 
-	printf("(Note -- the date should not be before 01-01-1700)\n");
+	printf("Input the date in the format dd-mm-yyyy :\n");
   scanf("%d-%d-%d", &d, &m, &y);
 
 	a = y % 100;
 	b = y / 100;
 	sum =	a + a/4 + d;
 	
-	if((m == 4) || (m == 7) || (m == 1 && (y%4 == 0 && y%100 != 0 || y&400 == 0 )))
+  int month_sum();
+	int century_sum();
+
+	i = new%7;
+	char day_of_week();
+
+	return 0;
+}
+
+
+int month_sum(int m)
+{
+	if((m == 1 && (y%4 == 0 && y%100 != 0 || y&400 == 0 )) || (m == 4) || (m == 7)) 
 		n_sum = sum;
 	else if(m == 1 || m == 10)
 		n_sum  = sum + 1;
@@ -29,11 +41,17 @@ int main()
 		n_sum = sum + 6;
 	else 
 		printf("Wrong format, please try again!!");
+
 		
+}
+
+
+int century_sum(int b)
+{	
 	if(b == 17)		
 		new = n_sum + 4;
 	else if(b == 18)
-	  new = n_sum + 2;
+	 	new = n_sum + 2;
 	else if(b == 19)
 		new = n_sum;
 	else if(b == 20)
@@ -41,26 +59,28 @@ int main()
 	else
 		printf("Wrong format, please try again!!");
 
-	
+}
+
+char day_of_week(int i)
+{
 	i = new%7;
 	printf("THE DAY OF THE WEEK OF %d-%d-%d : ", d, m, y);
-
-	if(i==0)
-		printf("SATURDAY");
-	else if(i==1)
-		printf("SUNDAY");
-	else if(i==2)
-		printf("MONDAY");
-	else if(i==3)
-		printf("TUESDAY");
-	else if(i==4)
-		printf("WEDNESDAY");
-	else if(i==5)
-		printf("THURSDAY");
-	else
-		printf("FRIDAY");
-
-
-	return 0;
+	
+	if(i == 0)
+		printf("Saturday");
+	else if(i == 1)
+		printf("Sunday");
+	else if(i == 2)
+		printf("Monday");
+	else if(i == 3)		
+		printf("Tuesday");
+	else if(i == 4)
+		printf("Wednesday");
+	else if(i == 5)
+		printf("Thursday");
+	else 
+		printf("Friday");
+		
 }
+
 
